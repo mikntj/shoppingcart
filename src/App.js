db9a5c28ca9780866455327dbedf34a233642702
Teddi <teddi@Teddis-MacBook-Pro.local>
2020-03-15 00:05:24 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+class App extends Component {
+  render() {
+    return (
+      {Router}
+      <div className="App">
+       <NavBar />
+      </div>
+      {/Router}
+    )
+  }
+}
+/*function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}*/
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+/*import React from 'react';
+import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import NavBar from './components/Todos';
+
+
+ const Navbar = ()=>{
+    return(
+            <nav className="nav-wrapper">
+                <div className="container">
+                    <Link to="/" className="brand-logo">Shopping</Link>
+                    
+                    <ul className="right">
+                        <li><Link to="/">Shop</Link></li>
+                        <li><Link to="/cart">My cart</Link></li>
+                        <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
+                    </ul>
+                </div>
+            </nav>  
+    )
+}
+
+export default Navbar */
+
+
+import React from 'react';
+
+/*import React, { Component } from 'react';*/
+
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div>
+       <h1>App HELLO</h1>
+      </div>
+    )
+  }
+}
+/*function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}*/
+
+export default NavBar;
